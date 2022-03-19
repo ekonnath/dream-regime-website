c75a9f7dc7ff44c4a45100d0bd8fb2e57e35c030
Erik Konnath <konnatherik@gmail.com>
2022-03-19 13:32:53 -0500

    Updated page locator method to work for GitHub

--- scripts/easter-egg-new.js
+++ scripts/easter-egg-new.js
@@ -5,8 +5,7 @@ class Egg {
     }
 }
 
-// let page = (location.pathname).substring(4, location.pathname.indexOf('.'));
-let page = location.pathname;
+let page = (location.pathname).substring(location.pathname.indexOf('.io/') + 4, location.pathname.indexOf('.html'));
 console.log('page: ' + page);
 
 const eggObjects = hatchEggs(); // egg objects
